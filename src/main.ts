2ec5ccd8b5f3dc1cb52107c062c983e5d23ecc0c
RSAdrian9 <adrian4rs@gmail.com>
2025-05-23 07:17:55 +0200

    Add: dependencies

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import App from "./App.vue";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import './assets/main.css'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
@@ -12,5 +15,6 @@ const options = {
     // You can set your default options here
 };
 app.use(router);
+app.use(ElementPlus)
 app.mount("#app");
 app.use(Toast, options);
